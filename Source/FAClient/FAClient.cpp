ff039a2631c17eb3ac8ec4cff38701ab29845930
yju123727 <yju123727@gmail.com>
2022-01-18 22:07:00 +0900

    - init

---
+++ Source/FAClient/FAClient.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FAClient.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FAClient, "FAClient" );
---
+++ Source/FAClient/FAClientGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FAClientGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FACLIENT_API AFAClientGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
